dfd5acf930776d1cc7a8a4fff7c32050b2d74456
programmer-man <bryan@kerigan.com>
2018-08-22 12:30:29 -0400

    feature box edit

--- wp-content/themes/kma-slim/template-parts/sections/feature-boxes.php
+++ wp-content/themes/kma-slim/template-parts/sections/feature-boxes.php
@@ -6,7 +6,7 @@
     </div>
     <div class="column is-6-tablet is-4-widescreen has-text-centered building-code is-flex-row is-justified">
         <div class="feature-box is-full-height is-flex-column is-justified-between is-aligned">
-            <h3 class="bebas">Building Code Services</h3>
+            <h3 class="bebas">Building Code Services<br>&nbsp;</h3>
             <p>Whether it is your home or your business, we always take a personal interest in each step of construction
                 to assure a safely established environment. </p>
             <a class="button is-outlined is-rounded is-caps is-bold" href="/services/">Learn More</a>
@@ -14,7 +14,7 @@
     </div>
     <div class="column is-4-widescreen has-text-centered fire-safety is-flex-row is-justified-start">
         <div class="feature-box is-full-height is-flex-column is-justified-between is-aligned">
-            <h3 class="bebas">Fire Safety Inspections</h3>
+            <h3 class="bebas">Building & Fire Safety Inspections</h3>
             <p>Under state and local laws, businesses must abide by fire codes for the safety of business employees, customers, and the surrounding properties.</p>
             <a class="button is-outlined is-rounded is-caps is-bold" href="/services/#fire-safety">Schedule an Inspection</a>
         </div>
