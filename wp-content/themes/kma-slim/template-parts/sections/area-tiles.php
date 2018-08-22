9e2582bcc08d1508dc682d125000ee6610b4cb17
programmer-man <bryan@kerigan.com>
2018-08-22 09:21:28 -0400

    wip

--- wp-content/themes/kma-slim/template-parts/sections/area-tiles.php
+++ wp-content/themes/kma-slim/template-parts/sections/area-tiles.php
@@ -8,7 +8,7 @@ $areas = new Areas();
         <h2 class="has-text-centered">City Information</h2>
         <p class="has-text-centered">Click below for your city's contractor and homeowner information and forms.</p>
     </div>
-    <div class="columns is-multiline is-gapless is-centered">
+    <div class="columns is-multiline is-gapless">
         <?php foreach($areas->getAreas() as $area){
             include(locate_template('template-parts/partials/mini-area.php'));
         } ?>
